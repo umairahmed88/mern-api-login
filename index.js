745ee05fae188a4be7efed40b859e0e0bbc8d8ee
umairahmed88 <umairahmed_88@ymail.com>
2024-08-21 14:09:30 +0500

    updating api for deploying on render

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.get("/", (req, res) => {
 	res.send("API is running");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-	console.log(`Listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Listening on port ${process.env.PORT}`);
 });
