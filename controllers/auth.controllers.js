c4c851562ed0f89b8b702f637d1f04d100e0bae2
umairahmed88 <umairahmed_88@ymail.com>
2024-08-25 02:29:17 +0500

    updated

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,10 +1,6 @@
 import jwt from "jsonwebtoken";
 import Auth from "../models/auth.model.js";
 import bcryptjs from "bcryptjs";
-import sgMail from "@sendgrid/mail";
-import "dotenv/config.js";
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const signup = async (req, res) => {
 	try {
@@ -31,50 +27,14 @@ export const signup = async (req, res) => {
 
 		await newUser.save();
 
-		const isProduction = process.env.NODE_ENV === "production";
-		const jwtSecret = isProduction
-			? process.env.PROD_JWT_SECRET
-			: process.env.DEV_JWT_SECRET;
-		const verificationToken = jwt.sign({ id: newUser._id }, jwtSecret, {
-			expiresIn: "1h",
-		});
-
-		const verificationLink = `https://ua-mern-api-login.vercel.app/api/v1/auth/verify-email?token=${verificationToken}`;
-
-		const msg = {
-			to: newUser.email,
-			from: {
-				name: "UA",
-				email: process.env.FROM_EMAIL,
+		res.status(201).json({
+			user: {
+				id: newUser._id,
+				username: newUser.username,
+				email: newUser.email,
 			},
-			subject: "Verify Your Email",
-			text: `Hello ${newUser.username}, please verify your email by clicking on the following link: ${verificationLink}`,
-			html: `<p>Hello ${newUser.username},</p><p>Please verify your email by clicking on the following link:</p><a href="${verificationLink}">Verify Email</a>`,
-		};
-
-		console.log(
-			"SendGrid API Key Loaded:",
-			process.env.SENDGRID_API_KEY ? "Yes" : "No"
-		);
-
-		sgMail
-			.send(msg)
-			.then(() => {
-				console.log("Verification email sent");
-				res.status(201).json({
-					user: {
-						id: newUser._id,
-						username: newUser.username,
-						email: newUser.email,
-					},
-					message:
-						"User registered successfully! Please check your email to verify your account.",
-				});
-			})
-			.catch((error) => {
-				console.error("Error sending email:", error);
-				res.status(500).json({ message: "Error sending verification email" });
-			});
+			message: "User registered successfully!",
+		});
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
