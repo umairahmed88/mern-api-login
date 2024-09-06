5fb9940b942418ad79d7dd96d03251bc7554e2be
umairahmed88 <umairahmed_88@ymail.com>
2024-09-06 14:53:25 +0500

    updated controllers

--- models/auth.model.js
+++ models/auth.model.js
@@ -5,7 +5,7 @@ const authSchema = new mongoose.Schema(
 		username: { type: String, required: true },
 		email: { type: String, required: true },
 		password: { type: String, required: true },
-		avatar: { type: String },
+		avatar: { type: String, default: "" },
 		isVerified: { type: Boolean, default: false },
 		purchaseHistory: [
 			{ type: mongoose.Schema.Types.ObjectId, ref: "ordersMern" },
