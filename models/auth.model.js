fdbe10411846f7e9f6a762b2d8d36a3c9e39a681
umairahmed88 <umairahmed_88@ymail.com>
2024-09-07 02:50:58 +0500

    updated auth model

--- models/auth.model.js
+++ models/auth.model.js
@@ -5,7 +5,7 @@ const authSchema = new mongoose.Schema(
 		username: { type: String, required: true },
 		email: { type: String, required: true },
 		password: { type: String, required: true },
-		avatar: { type: String, default: "" },
+		avatar: { type: String, default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" },
 		isVerified: { type: Boolean, default: false },
 		purchaseHistory: [
 			{ type: mongoose.Schema.Types.ObjectId, ref: "ordersMern" },
